d1341f2ebf1583f8b7b6396ef2452430a78353f7
Zeken3205 <2006139@kiit.ac.in>
2023-11-23 10:35:03 +0530

    2nd model done

---
+++ index.js
@@ -0,0 +1,18 @@
+const connectDB = require('./db');
+const express = require('express')
+var cors = require('cors')
+connectDB();
+const app = express()
+const port = 5000
+
+app.use(cors())
+
+//middleware
+app.use(express.json())
+
+//routes
+app.use('/api/auth', require('./routes/auth'))
+
+app.listen(port, () => {
+    console.log(`Inotebook backend listening at http://localhost:${port}`)
+})
